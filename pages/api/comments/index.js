9475890657ac5b497b1a85af0b2eb1bb886f24dd
Srijan Shovit <srijanshovit@gmail.com>
2021-12-03 10:54:29 +0530

    post

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,5 +1,27 @@
 import { comments } from "../../../data/comments";
 
 export default function handler(req, res) {
-    res.status(200).json(comments)
+
+    //handling get requests
+    if  (req.method === "GET") {
+        res.status(200).json(comments)
+    }
+
+    //handling post requests
+    else if (req.method === "POST") {
+        //extract comment from request's body
+        const comment = req.body.comment
+
+        //making new comment 
+        const newComment = {
+            id:Date.now(),
+            body:comment
+        }
+
+        //push it to array of comments
+        comments.push(newComment)
+
+        //send response
+        res.status(201).json(newComment)
+    }
 }
